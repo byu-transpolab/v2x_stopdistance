1820a90a03c1df42404a811b51c340e822e830bf
Shannon Andersen <shannon.andersen15@gmail.com>
2021-10-02 13:28:44 -0600

    Started a function that maps speed data onto centerline data, but I'm going to put that function on pause and continue splitting up my existing code into smaller functions.

--- R/functions.R
+++ R/functions.R
@@ -26,8 +26,31 @@ move2curve <- function(cx1, cy1, cx2, cy2, lng, lat){
   return(n_point)
 }
 
+move2curve(-111.7228, 40.6336, -111.7226, 40.63341, -111.7226, 40.63344)
 
-move2curve(-111.7228, 40.6336, -111.7226, 40.63341, 40.63344, -111.7226)
+filter_distance <- function()
+
+MapPoints <- function( center_df, data_df, east_west ){
+  a=1
+  new_df <- data.frame(LONGITUDE = numeric(),
+                       LATITUDE = numeric())
+  if (east_west == east){
+   for (i in 1:length(center_df)-1){
+     for (j in 1:length(data_df)){
+       move2curve(center_df$E_LONG[i], 
+                  center_df$E_LAT[i], 
+                  center_df$E_LONG[i+1],
+                  center_df$E_LAT[i+1],
+                  data_df$LONGITUDE[j],
+                  data_df$LATITUDE[j])
+       new_df$LONGITUDE[a]<-n_point[1]
+       new_df$LATITUDE[a]<-n_point[1]
+       a=a+1
+       
+   
+   }}
+}}
+
 #' Measure distance along curve
 #' 
 #' @param point A single point, previously moved to the curve
