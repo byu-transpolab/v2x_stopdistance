41feb63731cfa85afa651c2a83c0da0b9caac04c
Shannon Andersen <shannon.andersen15@gmail.com>
2021-11-05 18:02:39 -0600

    Changed for loop to mapply function and it works without using extra cores!

--- R/functions.R
+++ R/functions.R
@@ -23,6 +23,15 @@ move2curve <- function(cx1, cy1, cx2, cy2, lng, lat){
     center_distance <- sqrt((nx - lng)^2 + (ny - lat)^2)*364000
     return(c(nx, ny, center_distance))
 }
+#' Function to create variables used in mapping points
+#' @param lead_lng leading centerline point longitude
+#' @param lead_lat leading centerline point latitude
+#' @param end_lng end centerline point longitude
+#' @param end_lat end centerline point latitude
+#' @param df data frame for new points
+collect_data <- function(lead_lng, lead_lat, end_lng, end_lat, df){
+  
+}
 
 #' Function to create a new data frame by mapping points onto a curve and adding them in two new columns
 #' @param new_df the name of the new data frame being created
@@ -32,35 +41,36 @@ add_points_to_df <-function(new_df, old_df, cp){
   cp <- as.numeric(cp)
   new_df <-as.data.frame(old_df)
   old_df <-as.data.frame(old_df)
-  new_df<-mutate(new_df, center_x =new_df$LONGITUDE-new_df$LONGITUDE,
-                 new_df, center_y =new_df$LATITUDE-new_df$LATITUDE,
+  new_df<-mutate(new_df, center_lng =new_df$LONGITUDE-new_df$LONGITUDE,
+                 new_df, center_lat =new_df$LATITUDE-new_df$LATITUDE,
                  new_df, center_d =new_df$LATITUDE-new_df$LATITUDE)
-  for(i in 1:length(old_df$LATITUDE)){
-    cx1=S_Curve_Centerlines$W_LONG[cp+1]
-    cy1=S_Curve_Centerlines$W_LAT[cp+1]
-    cx2=S_Curve_Centerlines$W_LONG[cp]
-    cy2=S_Curve_Centerlines$W_LAT[cp]
-    lng=new_df$LONGITUDE[i]
-    lat=new_df$LATITUDE[i]
-    output<-move2curve(cx1, cy1, cx2, cy2, lng, lat)
-    a=output[1]
-    b=output[2]
-    c=output[3]
-    new_df$center_x[i]=a
-    new_df$center_y[i]=b
-    new_df$center_d[i]=c
-  }
-  # new_df<-filter(new_df,
-  #         center_d<30,
-  #         LONGITUDE<=cx1,
-  #         LATITUDE>=cy1)
-  # new_df<-group_by(new.df, TEMP_ID)
-  # new_df<-mutate(new.df,
-  #                ELEVATION_CHANGE = lead(ELEVATION_FT)-ELEVATION_FT)
-  # new_df<-ungroup(new_df, TEMP_ID)
-  # new_df<-filter(new_df, ELEVATION_CHANGE<0)
-  # return(new_df)
-}
+  CX1=S_Curve_Centerlines$W_LONG[cp+1]
+  CY1=S_Curve_Centerlines$W_LAT[cp+1]
+  CX2=S_Curve_Centerlines$W_LONG[cp]
+  CY2=S_Curve_Centerlines$W_LAT[cp]
+  new_points<-mapply(FUN = move2curve, 
+                     cx1=CX1, 
+                     cy1=CY1, 
+                     cx2=CX2, 
+                     cy2=CY2,
+                     lng=new_df$LONGITUDE, 
+                     lat=new_df$LATITUDE)
+  new_points <- t(new_points)
+  new_points<- as.data.frame(new_points)
+  new_df$center_lng<-new_points$V1
+  new_df$center_lat<-new_points$V2
+  new_df$center_d<-new_points$V3
+  new_df<-filter(new_df,
+          center_d<30,
+          LONGITUDE<=CX1,
+          LATITUDE>=CY1)
+  new_df<-group_by(new_df, TEMP_ID)
+  new_df<-mutate(new_df,
+                 ELEVATION_CHANGE = lead(ELEVATION_FT)-ELEVATION_FT)
+  new_df<-ungroup(new_df, TEMP_ID)
+  new_df<-filter(new_df, ELEVATION_CHANGE<0)
+  return(new_df)
+ }
 
 #' Transforms latitude/longitude data into sf
 transform_sf <- function(df){
@@ -79,8 +89,8 @@ transform_sf <- function(df){
 my_add_circles <-function(map, DF, COLOR, RAD=0.5, GROUP, ...){
   addCircles(map,
             data = DF,
-            lng = as.numeric(DF$LONGITUDE),
-            lat = as.numeric(DF$LATITUDE),
+            lng = as.numeric(DF$center_lng),
+            lat = as.numeric(DF$center_lat),
             color = COLOR,
             radius = RAD,
             group = GROUP,
